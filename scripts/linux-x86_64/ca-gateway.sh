309173d6cb18ecefa4cf3d7dfe6ed3df164d05dc
Roberto Rojas <roberto.rojas@noirlab.edu>
2024-05-28 20:50:10 +0000

    Change configuration files

--- scripts/linux-x86_64/ca-gateway.sh
+++ scripts/linux-x86_64/ca-gateway.sh
@@ -17,13 +17,27 @@ elif [ "$MKO" != "" ]; then
   TCS_IP=$MKO_TCS_IP
 elif [ "$CPO" != "" ]; then
   TCS_IP=$CPO_TCS_IP
+  GEN_IP=$CPO_GEN_IP
+  AG_IP=$CPO_AG_IP
 else
   TCS_IP=$DEFAULT_IP
 fi
 
 $(dirname $0)/../../bin/linux-x86_64/gateway -pvlist /gem_conf/rt/cagateway/GATEWAY.pvlist \
                       -access /gem_conf/rt/cagateway/GATEWAY.access \
+                      -log /var/log/ca-gateway/gengateway.log \
+                      -prefix gengate -archive -no_cache -debug 1 \
+                      -cip "${GEN_IP}" -report /var/log/ca-gateway/gengateway.txt 
+
+$(dirname $0)/../../bin/linux-x86_64/gateway -pvlist /gem_conf/rt/cagateway/GATEWAY-TCS.pvlist \
+                      -access /gem_conf/rt/cagateway/GATEWAY.access \
                       -log /var/log/ca-gateway/tcsgateway.log \
-                      -prefix tcsgate -archive -no_cache -debug 1 \
-                      -cip "${TCS_IP}" -report /var/log/ca-gateway/gateway.txt -prefix gateway
+                      -prefix tcssgate -archive -no_cache -debug 1 \
+                      -cip "${TCS_IP}" -report /var/log/ca-gateway/tcsgateway.txt 
+
+$(dirname $0)/../../bin/linux-x86_64/gateway -pvlist /gem_conf/rt/cagateway/GATEWAY-AG.pvlist \
+                      -access /gem_conf/rt/cagateway/GATEWAY.access \
+                      -log /var/log/ca-gateway/AGgateway.log \
+                      -prefix aggate -archive -no_cache -debug 1 \
+                      -cip "${AG_IP}" -report /var/log/ca-gateway/aggateway.txt 
 
