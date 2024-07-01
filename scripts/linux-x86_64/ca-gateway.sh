806d1de14bfd6881657b75f135d2da058332dbf6
Roberto Rojas <roberto.rojas@noirlab.edu>
2024-07-01 20:26:53 +0000

    update start up command

--- scripts/linux-x86_64/ca-gateway.sh
+++ scripts/linux-x86_64/ca-gateway.sh
@@ -18,26 +18,12 @@ elif [ "$MKO" != "" ]; then
 elif [ "$CPO" != "" ]; then
   TCS_IP=$CPO_TCS_IP
   GEN_IP=$CPO_GEN_IP
-  AG_IP=$CPO_AG_IP
 else
   TCS_IP=$DEFAULT_IP
 fi
 
 $(dirname $0)/../../bin/linux-x86_64/gateway -pvlist /gem_conf/rt/cagateway/GATEWAY.pvlist \
                       -access /gem_conf/rt/cagateway/GATEWAY.access \
-                      -log /var/log/ca-gateway/gengateway.log \
-                      -prefix gengate -archive -no_cache -debug 1 \
-                      -cip "${GEN_IP}" -report /var/log/ca-gateway/gengateway.txt 
-#
-#$(dirname $0)/../../bin/linux-x86_64/gateway -pvlist /gem_conf/rt/cagateway/GATEWAY-TCS.pvlist \
-#                      -access /gem_conf/rt/cagateway/GATEWAY.access \
-#                      -log /var/log/ca-gateway/tcsgateway.log \
-#                      -prefix tcssgate -archive -no_cache -debug 1 \
-#                      -cip "${TCS_IP}" -report /var/log/ca-gateway/tcsgateway.txt &
-#
-#$(dirname $0)/../../bin/linux-x86_64/gateway -pvlist /gem_conf/rt/cagateway/GATEWAY-AG.pvlist \
-#                      -access /gem_conf/rt/cagateway/GATEWAY.access \
-#                      -log /var/log/ca-gateway/AGgateway.log \
-#                      -prefix aggate -archive -no_cache -debug 1 \
-#                      -cip "${AG_IP}" -report /var/log/ca-gateway/aggateway.txt &
-#
+                      -log /var/log/ca-gateway/gateway.log \
+                      -prefix gate -archive -no_cache -debug 1 \
+                      -cip "${GEN_IP}" -report /var/log/ca-gateway/gateway.txt 
